d1a0b524ee3fc0df1f334cca762ffdd08c00bb75
u_sugane <sugane.ns10@gmail.com>
2025-03-27 20:06:44 +0900

    コードジェネレートのためのプロジェクトを作成した。

---
+++ CSStack.PrimeBlazor.PageManager/PagePathGenerator.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace CSStack.PrimeBlazor.PageManager
+{
+    [Generator(LanguageNames.CSharp)]
+    public sealed class PagePathGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput(
+                static x =>
+                {
+                    x.AddSource(
+                        "GeneratedBlazorCode.cs",
+                        """
+                        namespace CSStack.PrimeBlazor.PageManager;
+                        using System;
+
+                        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+                        internal sealed class PagePathAttribute : Attribute
+                        {
+                            public string Path { get; }
+                            public PagePathAttribute(string path)
+                            {
+                                Path = path;
+                            }
+                        }
+                        """);
+                });
+        }
+    }
+}
