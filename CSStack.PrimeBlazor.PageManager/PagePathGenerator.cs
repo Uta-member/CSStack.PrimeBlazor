04b66727aa3cd9235f03c7bbe931faec15e1e4ca
u_sugane <sugane.ns10@gmail.com>
2025-03-28 09:39:04 +0900

    ページマネージャテストアプリを作成し、デバッグができるところまで

--- CSStack.PrimeBlazor.PageManager/PagePathGenerator.cs
+++ CSStack.PrimeBlazor.PageManager/PagePathGenerator.cs
@@ -7,26 +7,6 @@ namespace CSStack.PrimeBlazor.PageManager
     {
         public void Initialize(IncrementalGeneratorInitializationContext context)
         {
-            context.RegisterPostInitializationOutput(
-                static x =>
-                {
-                    x.AddSource(
-                        "GeneratedBlazorCode.cs",
-                        """
-                        namespace CSStack.PrimeBlazor.PageManager;
-                        using System;
-
-                        [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-                        internal sealed class PagePathAttribute : Attribute
-                        {
-                            public string Path { get; }
-                            public PagePathAttribute(string path)
-                            {
-                                Path = path;
-                            }
-                        }
-                        """);
-                });
         }
     }
 }
